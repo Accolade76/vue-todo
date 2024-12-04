9f7ab239b150da5b5e8ca020f51de66a1a208cb0
Saket Kulkarni <dev@cassk.nl>
2024-12-04 20:48:33 +0100

    Hardcode task list

--- src/views/ToDoView.vue
+++ src/views/ToDoView.vue
@@ -1,11 +1,13 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
- <h1>Task List</h1>
+  <h1>Task List</h1>
+  <div>
+    <ul>
+      <li>Task 1</li>
+      <li>Task 2</li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
