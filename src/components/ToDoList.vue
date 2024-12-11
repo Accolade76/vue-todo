3ac60dc99d9461d89c1960628d6704aaa7fa3fa8
Matteo Pierro <pierromatteo@gmail.com>
2024-12-11 21:44:00 +0200

    test when there are more todos in the view

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -15,7 +15,7 @@ export default {
 <template>
   <div data-test="todos">
     <ul>
-      <li v-for="task in tasks" :key="task.id">
+      <li v-for="task in tasks" :key="task.id" data-test="todo">
         {{ task.title}}
       </li>
     </ul>
--- src/components/__tests__/ToDoList.spec.ts
+++ src/components/__tests__/ToDoList.spec.ts
@@ -23,4 +23,24 @@ describe('TodoList', () => {
     const todos = wrapper.get('[data-test="todos"]');
     expect(todos.text()).toBe("Learning Vue")
   });
+
+  it('shows a list with more todos', () => {
+    const wrapper = mount(ToDoList, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            createSpy: vi.fn,
+            initialState: {
+              taskStore: { tasks: [{"id": 123, "title": "Learning Vue"}, {"id": 456, "title": "Learning Rust"}]}
+            },
+          }),
+        ],
+      },
+    });
+
+    const todos = wrapper.findAll('[data-test="todo"]');
+    expect(todos).toHaveLength(2);
+    expect(todos.at(0).text()).toBe("Learning Vue")
+    expect(todos.at(1).text()).toBe("Learning Rust")
+  });
 });
