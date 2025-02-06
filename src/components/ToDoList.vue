3cc212d542ad0c1a70612d2fa870f9b5c071ab15
Saket Kulkarni <dev@cassk.nl>
2025-02-06 21:12:29 +0100

    Replace export with setup by a script setup

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,22 +1,16 @@
-<script lang="ts">
-import { useTasksStore } from '@/stores/tasks.ts'
+<script setup lang="ts">
+import { useTasksStore } from '@/stores/tasks'
 import { storeToRefs } from 'pinia'
 
-export default {
-  name: 'ToDoList',
-  setup() {
-    const tasksStore = useTasksStore()
-    const { tasks } = storeToRefs(tasksStore)
-    return { tasks }
-  }
-}
+const tasksStore = useTasksStore()
+const { tasks } = storeToRefs(tasksStore)
 </script>
 
 <template>
   <div data-test="todos">
     <ul>
       <li v-for="task in tasks" :key="task.id" data-test="todo">
-        {{ task.title}}
+        {{ task.title }}
       </li>
     </ul>
   </div>
