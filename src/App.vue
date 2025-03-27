6d395810740176214478dbe7606573f36fc23a4a
Saket Kulkarni <dev@cassk.nl>
2025-03-27 21:10:40 +0100

    Introduce Inbox Screen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ToDoView from './views/ToDoView.vue'
+import InboxScreen from '@/components/InboxScreen.vue'
 </script>
 
 <template>
   <ToDoView />
+  <InboxScreen />
 </template>
 
 <style scoped>
