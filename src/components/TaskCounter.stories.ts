3ac33ef926ce8df4f67ff3b94dcacdfc5d19e617
Saket Kulkarni <dev@cassk.nl>
2025-05-29 21:16:38 +0200

    Add correct types to the TaskCounter.stories.ts

--- src/components/TaskCounter.stories.ts
+++ src/components/TaskCounter.stories.ts
@@ -1,14 +1,19 @@
 import TaskCounter from './TaskCounter.vue'
+import type { Meta, StoryObj } from '@storybook/vue3';
 import { expect, within } from '@storybook/test'
 
-export default {
+const meta: Meta<typeof TaskCounter> = {
   component: TaskCounter,
   title: 'Count Tasks',
-  tags: ['autodocs']
-}
+  tags: ['autodocs'],
+};
 
-export const Default = {};
-export const NoTasks = {
+export default meta;
+
+type Story = StoryObj<typeof TaskCounter>;
+
+export const Default: Story = {};
+export const NoTasks: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
 
